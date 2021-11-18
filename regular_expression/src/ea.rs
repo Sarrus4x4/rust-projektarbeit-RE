eb848059a00f1d7d21a138edb56b606ea713bd9d
David <david.berchtold@gmx.de>
2021-11-18 20:18:29 +0100

    Added documentation for the re.rs file and all its functions

--- regular_expression/src/ea.rs
+++ regular_expression/src/ea.rs
@@ -1,9 +1,14 @@
+
+
 pub struct Transition{
     pub from: i32,
     pub char: char,
     pub to: i32,
     pub epsilon: bool,
 }
+
+
+
 
 pub trait TransitionRules{
     fn transition(&self);
@@ -12,4 +17,8 @@ pub trait TransitionRules{
     fn to_state(&self)->i32;
     fn trigger(from: i32, char: char)->bool;
     fn eps_trigger(from: i32)->bool;
+
+}
+pub trait TransformWorker{
+    fn transformWorker(RE: str);
 }
--- regular_expression/src/re.rs
+++ regular_expression/src/re.rs
@@ -1,29 +1,32 @@
 // ######### Structs #########
+// This is where the different struct are defined 
 pub struct Eps {
-    //can it stay empty?
-//pub val: String
+    //The Eps object has no parameters 
 }
 pub struct Phi {
-    //can it stay empty?
-//pub val: String
+    //The Phi object has no parameters
 }
 pub struct C {
     pub val: String,
+    //The C struct has a value parameter of type String. Here the the smalest element of a RE is saved
 }
 pub struct Alt<T, J> {
     pub l: T,
     pub r: J,
+    //The Alt struct has tho parameters of a variable Type. They are the left and right part of the alternative clause (l|r)
 }
 pub struct Conc<T, J> {
     pub l: T,
     pub r: J,
+    //The Conc struct has tho parameters of a variable Type. They are the left and right part of the concatination clause (lr)
 }
 pub struct Star<T> {
     pub obj: T,
+    //The Star struct has a single parameter of variable Type. It is the argument which is supposed to get a "*" added to it like so: obj*
 }
 
 // ######### Traits #########
-
+//Here all the necessary traits are defined to format the RE's and to determine whether they contain Eps or are Phi 
 pub trait RE {
     fn pretty(&self) -> String;
     fn contains_eps(&self) -> bool;
@@ -31,141 +34,153 @@ pub trait RE {
 }
 
 // ######### Implementation #########
-
+//Here the RE trait is implemented for every struct that was defined at the start
 impl RE for C {
-    fn pretty(&self) -> String {
+
+    fn pretty(&self) -> String { //The pretty function for the C struct is simply returning the String value
         self.val.to_string()
     }
-    fn contains_eps(&self) -> bool {
+
+    fn contains_eps(&self) -> bool { //The contains_eps function is simply checking if the current argument is equal to the empty String
         let mut contains: bool = false;
         if self.val == "" {
             contains = true;
         }
         contains
     }
+    
     fn is_phi(&self) -> bool {
-        let mut contains: bool = false;
+        let mut contains: bool = false; //The is_phi function is simply checking if the current argument is equal to phi
         if self.pretty() == "phi" {
             contains = true;
         }
         contains
     }
+
 }
 
-impl<T: RE, J: RE> RE for Alt<T, J> {
-    fn pretty(&self) -> String {
+impl<T: RE, J: RE> RE for Alt<T, J> { 
+
+    fn pretty(&self) -> String { //The pretty function for the Alt struct is checking for different special cases and is formatting the result accordingly
         if self.l.contains_eps() || self.l.is_phi() {
             if self.r.contains_eps() || self.r.is_phi() {
-                format!("")
+                format!("") //If both the left and the right argument of the alternative Struct are either eps or phi than the whole term is empty
             } else {
-                format!("{}", self.r.pretty())
+                format!("{}", self.r.pretty()) //If just the left argument of the alternative Struct is either eps or phi than just the right argument is formatted (r1 + r2 ==> r2 if L(r1)={})
             }
         } else if self.r.contains_eps() || self.r.is_phi() {
-            format!("{}", self.l.pretty())
+            format!("{}", self.l.pretty()) //If just the right argument of the alternative Struct is either eps or phi than just the left argument is formatted (r1 + r2 ==> r1 if L(r2)={})
         } else {
             if self.l.pretty() == self.r.pretty(){
-                format!("{}", self.l.pretty()) 
+                format!("{}", self.l.pretty()) //If the left and right argument of the alternative Stuct are the same than just one of them is formatted (r + r ==> r)
             }else{
-                format!("({}|{})", self.l.pretty(), self.r.pretty())
+                format!("({}|{})", self.l.pretty(), self.r.pretty()) //If both arguments of the alternative Stuct are of regular type than they are formated like so: (l|r) 
             }
         }
     }
-    fn contains_eps(&self) -> bool {
+
+    fn contains_eps(&self) -> bool { //The contains_eps function is simply checking if the current argument is equal to the empty String
         let mut contains: bool = false;
-        if self.pretty() == "" { //.val
+        if self.pretty() == "" {
             contains = true;
         }
         contains
     }
-    fn is_phi(&self) -> bool {
+
+    fn is_phi(&self) -> bool { //The is_phi function is simply checking if the current argument is equal to phi
         let mut contains: bool = false;
         if self.pretty() == "phi" {
             contains = true;
         }
         contains
     }
+
 }
 
 impl<T: RE, J: RE> RE for Conc<T, J> {
-    fn pretty(&self) -> String {
+
+    fn pretty(&self) -> String { //The pretty function for the Conc struct is checking for different special cases and is formatting the result accordingly
         if self.l.is_phi() || self.r.is_phi() {
-            //if left or right argument of concat is Phi -> the whole term is Phi
-            format!("")
+            format!("") //If the left or the right argument of the concatination Struct are phi than the whole term is phi
         } else {
             if self.l.contains_eps() {
                 if self.r.contains_eps() {
-                    format!("")
+                    format!("") //If both the left and the right argument of the concatination Struct eps than the whole term is empty (phi if L(r1)={} or L(r2)={})
                 } else {
-                    format!("{}", self.r.pretty())
+                    format!("{}", self.r.pretty()) //If just the left argument of the concatination Struct is eps than only the right argument is formatted (eps r ==> r)
                 }
             } else if self.r.contains_eps() {
-                format!("{}", self.l.pretty())
+                format!("{}", self.l.pretty()) //If just the right argument of the concatination Struct is eps than only the left argument is formatted (r eps ==> r)
             } else {
-                format!("({}{})", self.l.pretty(), self.r.pretty()) //logic to check for phi and format accordingly
+                format!("({}{})", self.l.pretty(), self.r.pretty()) //If both arguments of the concatination Stuct are of regular type than they are formated like so: (lr) 
             }
         }
     }
-    fn contains_eps(&self) -> bool {
+
+    fn contains_eps(&self) -> bool { //The contains_eps function is simply checking if the current argument is equal to the empty String
         let mut contains: bool = false;
-        if self.pretty() == "" { //.val
+        if self.pretty() == "" {
             contains = true;
         }
         contains
     }
-    fn is_phi(&self) -> bool {
+
+    fn is_phi(&self) -> bool { //The is_phi function is simply checking if the current argument is equal to phi
         let mut contains: bool = false;
         if self.pretty() == "phi" {
             contains = true;
         }
         contains
     }
+
 }
 
 impl<T: RE> RE for Star<T> {
-    
-    fn pretty(&self) -> String {
-        
+
+    fn pretty(&self) -> String { //The pretty function for the Star struct is checking for different special cases and is formatting the result accordingly
         if self.obj.contains_eps() || self.obj.is_phi() {
-            //logic to check for eps/phi and format accordingly
-            format!("")
+            format!("") //If the obj argument of the star Struct is eps or phi than the whole term is empty
         } else {
             let s = self.obj.pretty();
-            if s.ends_with("*)"){ // logic for (r*)* ==> r*
-                format!("{}", self.obj.pretty())
+            if s.ends_with("*)"){
+                format!("{}", self.obj.pretty()) //If the star Struct is called twice in a row (nested call) than it is interpreted as just one call ((r*)* ==> r*)
             } else{
-                format!("({}*)", self.obj.pretty())  
+                format!("({}*)", self.obj.pretty()) //If the argument of the star Struct is of regular type than it is formated like so: (obj*)
             }
             
         }
     }
-    fn contains_eps(&self) -> bool {
+
+    fn contains_eps(&self) -> bool { //The contains_eps function is simply checking if the current argument is equal to the empty String
         let mut contains: bool = false;
-        if self.pretty() == "" { //.val
+        if self.pretty() == "" { 
             contains = true;
         }
         contains
     }
-    fn is_phi(&self) -> bool {
+
+    fn is_phi(&self) -> bool { //The is_phi function is simply checking if the current argument is equal to phi
         let mut contains: bool = false;
         if self.pretty() == "phi" {
             contains = true;
         }
         contains
     }
+
 }
 
 impl RE for Eps {
-    fn pretty(&self) -> String {
+    fn pretty(&self) -> String { //The pretty function for the Eps struct is simply formatting a empty String
         format!("")
     }
-    fn contains_eps(&self) -> bool {
+    fn contains_eps(&self) -> bool { //The contains_eps function is simply checking if the current argument is equal to the empty String
         let mut contains: bool = false;
-        if self.pretty() == "" { //val
+        if self.pretty() == "" { 
             contains = true;
         }
         contains
     }
-    fn is_phi(&self) -> bool {
+    fn is_phi(&self) -> bool { //The is_phi function is simply checking if the current argument is equal to phi
         let mut contains: bool = false;
         if self.pretty() == "phi" {
             contains = true;
@@ -175,17 +190,17 @@ impl RE for Eps {
 }
 
 impl RE for Phi {
-    fn pretty(&self) -> String {
+    fn pretty(&self) -> String { //The pretty function for the Phi struct is simply formatting the "phi" String 
         format!("phi")
     }
-    fn contains_eps(&self) -> bool {
+    fn contains_eps(&self) -> bool { //The contains_eps function is simply checking if the current argument is equal to the empty String
         let mut contains: bool = false;
-        if self.pretty() == "" { //.val
+        if self.pretty() == "" { 
             contains = true;
         }
         contains
     }
-    fn is_phi(&self) -> bool {
+    fn is_phi(&self) -> bool { //The is_phi function is simply checking if the current argument is equal to phi
         let mut contains: bool = false;
         if self.pretty() == "phi" {
             contains = true;
@@ -195,6 +210,7 @@ impl RE for Phi {
 }
 
 // ######### main method #########
+//Here i am testing different RE's for their correct formatting and simplification
 pub fn run() {
     let re1 = Alt {l: C {val: "a".to_string() }, r: C { val: "b".to_string() }};
     println!("{}", re1.pretty());
