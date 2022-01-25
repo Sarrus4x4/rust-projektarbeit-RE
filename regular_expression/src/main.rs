c51075d2f3e5938beb61383fa9dfb22952925aae
Sarrus4x4 <david.berchtold@gmx.de>
2022-01-25 21:20:04 +0100

    delete some files

--- regular_expression/src/main.rs
+++ regular_expression/src/main.rs
@@ -1,11 +1,14 @@
-mod ausdruck;
-mod automat;
-mod sprache;
+mod automata;
+mod expression;
+mod language;
 
-
-
-
+/*
+create a Regular Expression re 
+    -> create the automata nfa for that Expression 
+        -> check if a word is in the language created by that automata/expression (the word to check for is currently defined in language.rs)
+*/
 pub fn main(){
-    //ausdruck::main();
-    sprache::run(automat::run());
+
+    //each step will print its own result on the way
+    language::run(automata::run(expression::run()));
 }
